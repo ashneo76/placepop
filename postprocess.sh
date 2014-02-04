ab58b9b9f01752d6dc8cdb3e99da97575d005d4a
Ashish Shah <ashneo76@gmail.com>
2014-02-04 22:25:00 +0000

    Updated postprocess.sh to remove temp files

--- postprocess.sh
+++ postprocess.sh
@@ -1,3 +1,4 @@
 src_file=$1
 sort -u -k9 -t"|" $src_file > $src_file".sorted"
 grep -v True $src_file".sorted" | grep -v "|-1" > $src_file".filtered"  # remove unfound/closed restaurants
+rm -f $src_file".sorted"
